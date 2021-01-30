fe8660bfdf74213aba7ea88e34531a28b70db4d9
hormeku seyram <hormeku45@gmail.com>
2021-01-30 14:14:24 +0000

    second react site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+// const App = () => {
+//   return <h1>Codetrain is awesome, react works!!!</h1>;
+// };
+
+class App extends React.Component {
+  render() {
+    return <h1>codetrain is awesome, react works!!!</h1>;
+  }
+}
+
+export default App;
